1610b89b802fc29c967ba21aa74c9a94ccf86159
Dyniek <damian.kawerski@gmail.com>
2025-06-09 20:51:29 +0200

    Base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/damiankawerski.github.io/',
   plugins: [react(), tailwindcss()],
 })
